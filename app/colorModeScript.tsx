0a39064b7f410c021b13702aa36fcc62caee8128
moorels <sales@micropls.com>
2024-12-15 18:53:35 +1100

    c4

--- app/colorModeScript.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { ColorModeScript } from '@chakra-ui/react'
-import theme from './theme'
-
-export default function ChakraColorModeScript() {
-  return <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-}
