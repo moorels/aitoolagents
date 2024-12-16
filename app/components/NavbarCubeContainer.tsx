64379ed8fd438bbc7a0acdd924acba12896997da
moorels <sales@micropls.com>
2024-12-16 16:46:23 +1100

    b8

--- app/components/NavbarCubeContainer.tsx
+++ app/components/NavbarCubeContainer.tsx
@@ -2,17 +2,27 @@
 
 import { useEffect, useRef } from 'react'
 import NavbarCube2 from './NavbarCube2'
+import PixelTitle from './PixelTitle';
 
 export default function NavbarCubeContainer() {
   const containerRef = useRef<HTMLDivElement>(null)
 
   return (
     <div 
-      className="fixed left-0 w-[100px] h-[300px] z-40" 
+      className="fixed left-0 w-[100px] h-[100px] z-40" 
       ref={containerRef}
-      style={{ top: '80px' }}  // Position it just below the navbar
+      style={{ top: '100px' }}  // Position it just below the navbar
     >
-      <NavbarCube2 />
+      <PixelTitle />
+      <div 
+      className="fixed left-0 w-[100px] h-[100px] z-40" 
+      ref={containerRef}
+      style={{ top: '100px', left : '30px'  }}  // Position it just below the navbar
+    >
+<NavbarCube2 />
+    </div>
+      
+
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import TypewriterText from './components/TypewriterText';
 import AINodeGraph from './components/AINodeGraph';
 import HumanFigure3D from './components/HumanFigure3D';
-import PixelTitle from './components/PixelTitle';
+
 
 export default function Home() {
   return (
     <div className="pt-10">
-      <div className="py-14 ">
-        <PixelTitle />
+      <div className="py-2 ">
+       
       </div>
 
       <div className="relative z-10">
         <div className="min-h-screen p-8 sm:p-0">
           <main className="flex items-center justify-center h-full">
-            <div className="w-full max-w-7xl mx-auto flex items-center justify-between gap-8">
+            <div className="w-full max-w-7xl mx-auto flex items-center justify-between gap-8 px-8">
               <div className="w-[80px] h-[600px] relative">
                 <HumanFigure3D />
               </div>
